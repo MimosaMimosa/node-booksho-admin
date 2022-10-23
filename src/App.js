7cf18210dfe070f6258fcd2826bb7bccdfda37fd
MimosaMimosa <waiyanlin2991998@gmail.com>
2022-10-24 01:19:28 +0630

    initial

--- src/App.js
+++ src/App.js
@@ -5,27 +5,50 @@ import {
     createRoutesFromElements,
 } from 'react-router-dom';
 import Home from './pages/home/Home';
-import UserList from './pages/list/UserList';
+import List from './pages/list/List';
 import Login from './pages/login/Login';
 import New from './pages/new/New';
 import Single from './pages/single/Single';
+import { productInputs, userInputs } from './formSource';
+import './style/dark.scss';
+import { DarkModeContext } from './context/darkModeContext';
+import { useContext } from 'react';
 
 function App() {
+    const { darkMode } = useContext(DarkModeContext);
     const router = createBrowserRouter(
         createRoutesFromElements(
             <Route path='/'>
                 <Route index element={<Home />}></Route>/
                 <Route path='/login' element={<Login />}></Route>
-                <Route path='/user'>
-                    <Route index element={<UserList />}></Route>
+                <Route path='/users'>
+                    <Route index element={<List />}></Route>
                     <Route path=':userId' element={<Single />}></Route>
-                    <Route path='new' element={<New />}></Route>
+                    <Route
+                        path='new'
+                        element={
+                            <New inputs={userInputs} title='Add New User' />
+                        }
+                    />
+                </Route>
+                <Route path='products'>
+                    <Route index element={<List />} />
+                    <Route path=':productId' element={<Single />} />
+                    <Route
+                        path='new'
+                        element={
+                            <New
+                                inputs={productInputs}
+                                title='Add New Product'
+                            />
+                        }
+                    />
                 </Route>
             </Route>
         )
     );
     return (
-        <div className='App'>
+        <div className={darkMode ? 'app dark' : 'app'}>
             <RouterProvider router={router} />
         </div>
     );
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,9 @@
-const Login = () => {
-    return <div>Login</div>;
-};
+import "./login.scss"
 
-export default Login;
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
